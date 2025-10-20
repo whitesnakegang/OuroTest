93c54b0d52b2fdcb68b21e6147a0aa2c7d00c45d
신진우 <sjw0066@naver.com>
2025-10-20 14:49:44 +0900

    chore: 테스트용 레포 초기 생성

---
+++ src/main/java/com/c102/ourotest/OurotestApplication.java
@@ -0,0 +1,13 @@
+package com.c102.ourotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurotestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurotestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/c102/ourotest/OurotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c102.ourotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurotestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
