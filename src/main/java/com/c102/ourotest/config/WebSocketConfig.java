94fa0dde7110af01c55c5e61243773c47d82f563
심연수 <shenlianxiu@gmail.com>
2025-11-12 16:37:10 +0900

    chore: broker prefix 설정
    - 이유: try 요청(send)에 대한 tryId를 /user/queue/try에서 얻기 위함

--- src/main/java/com/c102/ourotest/config/WebSocketConfig.java
+++ src/main/java/com/c102/ourotest/config/WebSocketConfig.java
@@ -19,7 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+//        registry.setUserDestinationPrefix("/user");
+        registry.enableSimpleBroker("/topic", "/queue");
     }
 }
 
